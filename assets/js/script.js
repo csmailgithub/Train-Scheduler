87a5e31f1ba2cfbcc591c904467a45173362f489
Colin S <csmail1290@gmail.com>
2018-04-12 12:09:06 -0700

    left then right

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,50 @@ var config = {
     projectId: "trainschedule-4fe05",
     storageBucket: "trainschedule-4fe05.appspot.com",
     messagingSenderId: "799313740898"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+setInterval(function(){
+$('.current-time').html(moment().format('hh:mm:ss A'))
+}, 1000);
+
+
+// $('.content').hide();
+
+//   // Declare variables
+// var database = firebase.database();
+// var editTrainKey = '';
+// var fbTime = moment();
+// var newTime;
+
+var database = firebase.database();
+
+// 2. Button for adding New Trains
+$("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var empName = $("#employee-name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+
+  // Creates local "temporary" object for holding employee data
+  var newEmp = {
+    name: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate
   };
-  firebase.initializeApp(config);
 
-    setInterval(function(){
-    $('.current-time').html(moment().format('hh:mm:ss A'))
-  }, 1000);
+  // Uploads employee data to the database
+  database.ref().push(newEmp);
 
-
-  $('.content').hide();
+  // Logs everything to console
+  console.log(newEmp.name);
+  console.log(newEmp.role);
+  console.log(newEmp.start);
+  console.log(newEmp.rate);
   
 });
